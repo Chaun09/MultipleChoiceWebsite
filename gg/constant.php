7948001649f016939be108a743906fcb5643cd37
chouming <bangcac24@gmail.com>
2024-04-24 14:14:53 +0700

    first commit

---
+++ gg/constant.php
@@ -0,0 +1,12 @@
+<?php
+
+define('EMAIL', 'chau@gmail.com');
+define('PASSWORD', '090103');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS','');
+define('DB_NAME', 'quizzz');
+
+?>
